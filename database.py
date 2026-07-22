c73c5f0db5e02150d6c7a4fae4b6b325df0a4044
eyash24 <eyash.prasad24@gmail.com>
2026-07-22 22:45:23 +0530

    Done with database, next CRUD operations

--- database.py
+++ database.py
@@ -5,17 +5,18 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args=("check_same_thread": False),
+    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(
     autocommit=False, 
-    autoflash = False,
+    autoflush = False,
     bind=engine
 )
 
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     with SessionLocal() as db:
